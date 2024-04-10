4a569fb07904b37b88280758d2730f0847023264
steinada <110931286+steinada@users.noreply.github.com>
2024-04-10 19:25:53 +0300

    Delete what_to_watch directory

--- what_to_watch/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///C:\\Users\\stein\\PycharmProjects\\techTasks\\what_to_watch\\opinions_app\\db.sqlite3'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    ENV = 'development'
-    DEBUG = True
